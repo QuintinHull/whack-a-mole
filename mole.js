4100441c864b0707dbb4c2cca9cd60ae8c87dac5
Quintin Hull <quintinhull92@gmail.com>
2020-12-03 18:50:56 -0500

    working on winning the game feature

--- mole.js
+++ mole.js
@@ -5,21 +5,33 @@ window.addEventListener("DOMContentLoaded", () => {
   }, 0);
 
   let moleHeads = document.querySelectorAll(".wgs__mole-head");
+
   for (let moleHead of moleHeads) {
     moleHead.addEventListener("click", () => {
       moleHead.classList.add("wgs__mole-head--hidden");
+      moleHead.classList.add("wgs__mole-head--whacked");
     });
   }
 });
 
 function popUpRandomMole() {
-  let randomNum = getRandomIntInclusive(0, 7);
-  const moleHeads = document.querySelectorAll(".wgs__mole-head");
+  const moleHeads = document.querySelectorAll(
+    ".wgs__mole-head:not(.wgs__mole-head--whacked)"
+  );
+  if (moleHeads.length === 0) {
+    let el = document.createElement("h1");
+    let div = document.getElementsByClassName("for-h1");
+    el.innerHTML = "You won!";
+    div.appendChild(el);
+    return;
+  }
+
+  let randomNum = getRandomIntInclusive(0, moleHeads.length - 1);
   moleHeads[randomNum].classList.remove("wgs__mole-head--hidden");
 
   setTimeout(() => {
     hideMole(moleHeads[randomNum]);
-  }, 3000);
+  }, 2000);
 }
 
 function hideMole(mole) {
