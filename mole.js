212b25e405c74ec419688ac9fbdc283e802367a2
Quintin Hull <quintinhull92@gmail.com>
2020-12-03 17:33:08 -0500

    finished popping up moles section

--- mole.js
+++ mole.js
@@ -1,4 +1,33 @@
 // Write your JS here.
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    popUpRandomMole();
+  }, 0);
+});
+
+function popUpRandomMole() {
+  let randomNum = getRandomIntInclusive(0, 7);
+  const moleHeads = document.querySelectorAll(".wgs__mole-head");
+  moleHeads[randomNum].classList.remove("wgs__mole-head--hidden");
+
+  setTimeout(() => {
+    hideMole(moleHeads[randomNum]);
+  }, 1000);
+}
+
+function hideMole(mole) {
+  mole.classList.add("wgs__mole-head--hidden");
+
+  setTimeout(() => {
+    popUpRandomMole();
+  }, 1000);
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
 // window.addEventListener("DOMContentLoaded", () => {
 //   setInterval(() => {
 //     const moleHeads = document.querySelectorAll(".wgs__mole-head");
